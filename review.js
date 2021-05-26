7689196d4709d3073c7f9f186412a60804415738
khushal <totakhailkhushal@gmail.com>
2021-05-26 14:04:21 +0200

    review part one done

---
+++ review.js
@@ -0,0 +1,63 @@
+// NOTE! Pay close attention to creating code with good and consistent code style.
+
+
+// 1. Make a variable and assign an even number to that variable.Make sure that variable cannot be changed.
+
+const numbers = 10;
+
+// 2. Make a variable and assign a negative odd number to that variable.Make sure that variable cannot be changed.
+
+const oddNumber = -1;
+
+// 3. Make a variable and assign a string to that variable using double quotes.Make sure that variable cannot be changed.
+
+const justName = "Hello World";
+
+// 4. Make a variable and assign a string to that variable using single quotes.Make sure that variable cannot be changed.
+
+
+const singleQuote = 'Attention';
+
+// 5. Make a variable and assign a string to that variable using a template literal.Make sure that variable cannot be changed.
+
+const variable = `Template Literal`;
+
+
+// 6. Make a variable and assign an empty string to that variable using a template literal.Make sure that variable * can * be changed.
+
+let emptyString = ` `;
+emptyString = 7;
+
+
+
+
+// 7. Make a variable and assign a boolean to that variable using a template literal.Make sure that variable * can * be changed.
+
+// 8. Make a variable and assign a boolean to that variable using a template literal.Make sure that variable can not be changed.
+
+
+
+
+
+// 9. Print the`type` of a variable that has a number value.
+
+const number = (45);
+console.log(typeof number);
+
+
+
+
+
+// 10. Print the`type` of a variable that has a string value.
+
+const fullName = "khushal"
+console.log(typeof fullName);
+
+
+
+
+// 11. Print the`type` of a variable that has a boolean value.
+
+
+const boleanValue = true;
+console.log(typeof boleanValue);
